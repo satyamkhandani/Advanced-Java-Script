301591ccf352cfcf9dfae6d2041276c71e49735c
satyamkhandani <thesatyamkhandani@gmail.com>
2025-04-01 13:01:14 +0530

    function basics

---
+++ script.js
@@ -0,0 +1,19 @@
+// function sayHello(){
+//   let you=prompt("Your name?");
+//   console.log("Hello",you+"!");
+// }
+
+// sayHello();
+
+// let varContainingFunction = function() {
+//   let varInFunction = " I'm in function";
+//   console.log("hi there ", varInFunction);
+// };
+// varContainingFunction();
+
+function add( a,b)  {
+  return a+b ;
+
+}
+
+console.log(add(5,10));
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+function sayHello(){
+  let you=prompt("Your name?");
+  console.log("Hello",you+"!");
+}
+
+sayHello();
