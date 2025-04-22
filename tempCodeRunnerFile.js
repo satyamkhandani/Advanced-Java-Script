a4ae12a8bdbd94502220ddc556d1029f56a3f04b
satyamkhandani <thesatyamkhandani@gmail.com>
2025-04-22 11:50:14 +0530

    Class Started

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+
+// class Person{
+//   constructor(firstname,lastname){
+//     this.firstname=firstname;
+//     this.lastname=lastname;
+
+//   }
+// }
+// let p=new Person("Maaike");
+
+// console.log("hi",p.firstname,p.lastname);
